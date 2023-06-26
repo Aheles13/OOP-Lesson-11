5ec985fefb97512073ac7c9e78788de664394923
44 <stepaburkov@gmail.com>
2023-06-26 20:15:16 +0500

    Добавьте файлы проекта.

---
+++ OOP Lesson 11/Class exemple.cpp
@@ -0,0 +1,10 @@
+#include "Class exemple.h"
+
+int g = 9;
+
+int MyClass::count{ 0 };
+
+int MyClass::getCount()
+{
+	return count;
+}
---
+++ OOP Lesson 11/OOP Lesson 11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+
+	//Шаблоны в классах 
+	//Статические методы и поля 
+
+
+
+
+
+
+
+
+
+	return 0;
+}
